fed378b8160e8a4286f71ae24460057ef836fece
niola <niolan007@yandex.ru>
2025-09-20 09:44:59 +0300

    Add 2 tests: - standard_user - locked_out_user

---
+++ src/test/java/pages/LoginPage.java
@@ -0,0 +1,27 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage {
+    WebDriver browser;
+
+    public LoginPage(WebDriver browser) {
+        this.browser = browser;
+    }
+
+    private static final By USERNAME_INPUT = By.xpath("//*[@placeholder='Username']");
+    private static final By PASSWORD_INPUT = By.xpath("//*[@placeholder='Password']");
+    private static final By LOGIN_BTN = By.xpath("//*[@id='login-button']");
+
+    public void open() {
+        browser.get("https://www.saucedemo.com/");
+    }
+
+    public void inputLoginPassword(String login, String password) {
+        browser.findElement(USERNAME_INPUT).sendKeys(login);
+        browser.findElement(PASSWORD_INPUT).sendKeys(password);
+        browser.findElement(LOGIN_BTN).click();
+    }
+
+}
---
+++ src/test/java/test/LoginTest.java
@@ -0,0 +1,23 @@
+package test;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
+
+public class LoginTest extends BaseTest {
+    @Test
+    public void checkStandardUserLogin() {
+        loginPage.open();
+        loginPage.inputLoginPassword("standard_user", "secret_sauce");
+        assertTrue(browser.getCurrentUrl().contains("/inventory"));
+    }
+
+    @Test
+    public void checkLockedOutUserLogin() {
+        loginPage.open();
+        loginPage.inputLoginPassword("locked_out_user", "secret_sauce");
+        assertTrue(browser.findElement(By.xpath("//*[@data-test='error']")).isDisplayed());
+    }
+
+}
