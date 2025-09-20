c68bb21d1a9406b5575b045d4c11840ef2359833
niola <niolan007@yandex.ru>
2025-09-20 11:10:35 +0300

    Refactoring

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class LoginPage {
-    private WebDriver browser;
+    private final WebDriver browser;
     private static final By USERNAME_INPUT = By.xpath("//*[@placeholder='Username']");
     private static final By PASSWORD_INPUT = By.xpath("//*[@placeholder='Password']");
     private static final By LOGIN_BTN = By.xpath("//*[@id='login-button']");
